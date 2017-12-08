2a6176af3e97d0c2624325bf49ecdcd12aafbc9f
Michael Brooks <mikeyabrooks@gmail.com>
2017-12-08 13:51:51 -0800

    add bug fix for ajax cart total, removed commented out code fixed bug.  Also start of next ajax showing product details path

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -6,7 +6,7 @@ class OrderItemsController < ApplicationController
     @order.save
     session[:order_id] = @order.id
     respond_to do |format|
-      format.html { redirect_to 'products_path' }
+      format.html { redirect_to products_path }
       format.js
     end
   end
